e206ac43ca71312ac459a2d0dfe3e0cf13ee7120
ItsWip <pswaraj0614@gmail.com>
2024-12-05 23:19:06 +0530

    Update index.js

--- 05.sign_up_page/index.js
+++ 05.sign_up_page/index.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){
 
+    let pass= document.querySelector(".pass").value;
+    let confirm_pass= document.querySelector(".confirm_pass").value;
+    
+    console.log(pass)
 })
