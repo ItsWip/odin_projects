e013f75970463a73309242c1e023343c8fb0fa9a
ItsWip <pswaraj0614@gmail.com>
2024-12-07 16:39:31 +0530

    updated js file

--- 05.sign_up_page/index.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){
-
-    let pass= document.querySelector(".pass").value;
-    let confirm_pass= document.querySelector(".confirm_pass").value;
-    
-    console.log(pass)
-})
