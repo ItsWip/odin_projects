a0a34af1af426c56a826a59f0a8125a7cb7f6701
ItsWip <pswaraj0614@gmail.com>
2024-12-22 16:11:33 +0530

    working delete btn

--- 07.Library/index.js
+++ 07.Library/index.js
@@ -7,6 +7,11 @@ function Book(title,author,pages,read) {
     this.read= read;
 }
 
+function delete_book(i){
+    myLibrary.splice(i,1);
+    print_book();
+}
+
 function print_book(){
     let lab= document.querySelector("#labrary");
     lab.innerHTML="";
@@ -15,12 +20,18 @@ function print_book(){
         let print_book= document.createElement("div");
         print_book.setAttribute("class","card");
         print_book.innerHTML=`
-        <div class="card_title"> <p>
-            Title: ${my_book.title} <br>
-            Author: ${my_book.author}</p>
+        <div class="card_title">
+            <H3>Title: ${my_book.title}</H3>
+        </div>
+        <div class="card_body">
+            Author: ${my_book.author} <br>
+            Pages: ${my_book.page} <br>
+            ${my_book.read? "Read" : "Want to read"}
+        <div>
+        <div id="btns">
+            <button id="delete" onclick="delete_book(${i})"> Delete</button>
         </div>`
-        print_book.style.display="block";
-        console.log(my_book);
+
         lab.appendChild(print_book);
     }       
 }
@@ -29,7 +40,7 @@ function addBookToLibrary() {
     let title= document.getElementById("title").value;
     let author= document.getElementById("author").value;
     let pages= document.getElementById("pages").value;
-    let read= document.getElementById("read").cheacked;
+    let read= document.getElementById("read").checked;
     new_book= new Book(title,author,pages,read);
     myLibrary.push(new_book);
     print_book();
